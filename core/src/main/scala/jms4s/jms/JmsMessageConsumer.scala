f1fa25b0bc54f98d922d0094dac2b031309f4b55
Alessandro Zoffoli <alessandro.zoffoli@gmail.com>
2020-04-03 11:21:28 +0200

    Update receive without weird machinery

--- core/src/main/scala/jms4s/jms/JmsMessageConsumer.scala
+++ core/src/main/scala/jms4s/jms/JmsMessageConsumer.scala
@@ -1,16 +1,19 @@
 package jms4s.jms
 
-import cats.effect.{ Concurrent, ContextShift }
+import cats.effect.{ Concurrent, ContextShift, Sync }
+import cats.implicits._
 import javax.jms.MessageConsumer
-import jms4s.IOOps.interruptable
 
 class JmsMessageConsumer[F[_]: Concurrent: ContextShift] private[jms4s] (
   private[jms4s] val wrapped: MessageConsumer
 ) {
 
   val receiveJmsMessage: F[JmsMessage[F]] =
-    interruptable(force = true) {
-      val message = wrapped.receive()
-      new JmsMessage(message)
-    }
+    for {
+      recOpt <- Sync[F].delay(Option(wrapped.receiveNoWait()))
+      rec <- recOpt match {
+              case Some(message) => Sync[F].pure(new JmsMessage(message))
+              case None          => ContextShift[F].shift >> receiveJmsMessage
+            }
+    } yield rec
 }
